425aef320d1e6b87228e2d80144e1e214ff7790e
Sergios - Anestis Kefalidis <sergioskefalidis@gmail.com>
2024-10-16 10:36:54 +0300

    Add screen and nvidia-container-toolkit

--- setup_server.sh
+++ setup_server.sh
@@ -13,7 +13,7 @@ sudo apt update
 sudo apt dist-upgrade
 
 # Install utilities
-sudo apt -y install screenfetch nmap htop iotop nano vim git curl
+sudo apt -y install screenfetch nmap htop iotop nano vim git curl screen
 
 # Install basic C & C++ libraries and tools
 sudo apt -y install build-essential gdb valgrind
@@ -71,3 +71,11 @@ sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 ## Docker permissions
 sudo cp ./permissions /etc/sudoers.d/permissions
+
+# nvidia-container-toolkit
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey |sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+&& curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list \
+&& sudo apt-get update
+sudo apt-get install -y nvidia-container-toolkit
+sudo nvidia-ctk runtime configure --runtime=docker
+sudo systemctl restart docker
